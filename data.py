8ec44da530af00d8ea576f73fee9660cd2f90d0b
Mynor Lopez <mynlop15@gmail.com>
2020-10-16 18:06:50 -0600

    primer parte del proyecto

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, leo vel egestas sodales, diam nisl interdum leo, sed gravida nibh orci sit amet massa. Integer dictum eget purus in porta.',
+            'author': 'Mynor Lopez',
+            'create_date': '16-10-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, leo vel egestas sodales, diam nisl interdum leo, sed gravida nibh orci sit amet massa. Integer dictum eget purus in porta.',
+            'author': 'Santiago Lopez',
+            'create_date': '16-10-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, leo vel egestas sodales, diam nisl interdum leo, sed gravida nibh orci sit amet massa. Integer dictum eget purus in porta.',
+            'author': 'Santi Sandoval',
+            'create_date': '16-10-2020'
+        }
+    ]
+
+    return articles
