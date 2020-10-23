cd4453b0acc30e3ee02f092d4f9b479e7d0920a9
Mynor Lopez <mynlop15@gmail.com>
2020-10-22 20:03:18 -0600

    login, dashboard, messages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flaskext.mysql import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -61,9 +62,70 @@ def register():
         cur.close()
         flash('You are now registered and can login','success')
         return redirect(url_for('index'))
-        
 
     return render_template('register.html', form=form)
+
+#login
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        #get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        #create cursor
+        con = mysql.connect()
+        cur = con.cursor()
+
+        #get user by username 
+        result = cur.execute("SELECT * FROM users WHERE username =%s", [username])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data[4]
+            #compare password
+            if sha256_crypt.verify(password_candidate, password):
+                #passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = "Invalid login"
+                return render_template('login.html', error=error)
+            #close connection
+            cur.close()
+        else:
+            # app.logger.info('NO USER')
+            error = "Username not found"
+            return render_template("login.html", error=error)
+
+    return render_template('login.html')
+
+#check is user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+#logout
+@app.route('/logout')
+def logout():
+   session.clear()
+   flash('You are now loggued out', 'success')
+   return redirect(url_for('login'))
+
+#dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.secret_key = 'secreto123'
