23fc1214d73ee6f7b62e5341f2b87a095f676b27
Mynor Lopez <mynlop15@gmail.com>
2020-10-27 19:09:06 -0600

    edicion y eliminacion de posts

--- app.py
+++ app.py
@@ -181,6 +181,48 @@ def add_article():
         return redirect(url_for('dashboard'))
     return render_template('add_article.html', form=form) 
 
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+    con = mysql.connect()
+    cur = con.cursor()
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+    article = cur.fetchone()
+    form = ArticleForm(request.form)
+
+    form.title.data = article[1]
+    form.body.data = article[3]
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        # create cursor
+        con = mysql.connect()
+        cur = con.cursor()
+
+        cur.execute("UPDATE articles SET title=%s, body=%s WHERE id=%s",(title, body, id))
+
+
+        con.commit() 
+        cur.close()
+        flash('Article Updated', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('edit_article.html', form=form) 
+
+#delete article
+@app.route('/delete_article/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_article(id):
+    con = mysql.connect()
+    cur = con.cursor()
+
+    cur.execute("DELETE FROM articles WHERE id = %s", [id])
+
+    con.commit()
+    cur.close()
+    flash("Article deleted", 'success')
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.secret_key = 'secreto123'
